1d9c9f14b11d35b8bae62c7051c2d01489e4eeb1
Gabriel Soares <gabrielsoares@Gabriels-MBP.fritz.box>
2025-08-14 00:50:46 +0200

    feat: starting project on day 2

---
+++ server.ts
@@ -0,0 +1,46 @@
+import fastify from 'fastify'
+import { fastifySwagger } from '@fastify/swagger'
+import { validatorCompiler, serializerCompiler, type ZodTypeProvider, jsonSchemaTransform } from 'fastify-type-provider-zod'
+import scalarAPIReference from '@scalar/fastify-api-reference'
+import { createCourseRoute } from './src/routes/create-course'
+import { getCourseByIdRoute } from './src/routes/get-course-by-id'
+import { getCoursesRoute } from './src/routes/get-courses'
+
+const server = fastify({
+  logger: {
+    transport: {
+      target: 'pino-pretty',
+      options: {
+        translateTime: 'HH:MM:ss Z',
+        ignore: 'pid,hostname',
+      },
+    },
+  },
+}).withTypeProvider<ZodTypeProvider>()
+
+if (process.env.NODE_ENV === 'development') {
+  server.register(fastifySwagger, {
+    openapi: {
+      info: {
+        title: 'Desafio Node.js',
+        version: '1.0.0',
+      }
+    },
+    transform: jsonSchemaTransform,
+  })
+  
+  server.register(scalarAPIReference, {
+    routePrefix: '/docs',
+  })
+}
+
+server.setValidatorCompiler(validatorCompiler)
+server.setSerializerCompiler(serializerCompiler)
+
+server.register(createCourseRoute)
+server.register(getCourseByIdRoute)
+server.register(getCoursesRoute)
+
+server.listen({ port: 3333 }).then(() => {
+  console.log('HTTP server running!')
+})
---
+++ src/routes/create-course.ts
@@ -0,0 +1,28 @@
+import type { FastifyPluginAsyncZod } from 'fastify-type-provider-zod'
+import z from 'zod'
+import { courses } from '../database/schema'
+import { db } from '../database/client'
+
+export const createCourseRoute: FastifyPluginAsyncZod = async (server) => {
+  server.post('/courses', {
+    schema: {
+      tags: ['courses'],
+      summary: 'Create a course',
+      body: z.object({
+        title: z.string().min(5, 'Título precisa ter 5 caracteres'),
+      }),
+      response: {
+        201: z.object({ courseId: z.uuid() }).describe('Curso criado com sucesso!')
+      }
+    },
+  }, async (request, reply) => {
+    const courseTitle = request.body.title
+  
+    const result = await db
+      .insert(courses)
+      .values({ title: courseTitle })
+      .returning()
+  
+    return reply.status(201).send({ courseId: result[0].id })
+  })
+}
